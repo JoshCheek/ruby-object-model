c368453adc449adc67529d4983636dad1b06c1cb
Josh Cheek <josh.cheek@gmail.com>
2014-08-19 13:28:29 -0600

    Increase clarity between 8.1 and 8.2

--- 8.2-memoization.rb
+++ 8.2-memoization.rb
@@ -1,5 +1,5 @@
 # ===== Memoization =====
 #
-# 1. Soooo, what's the issue? Why is this happening?
+# 1. Soooo, what's the issue with all the instances? Why is this happening?
 # 2. Given that we know this, what might we do about it?
 # 3. How can we check that it works?
